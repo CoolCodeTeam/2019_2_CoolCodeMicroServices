9e9596947f6597cd87ffd9421e2c80f03251e6ed
alexey <aleksma99@yandex.ru>
2019-11-26 22:44:04 +0300

    [FIX]

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-gnome-terminal -- go run ./users/
-gnome-terminal -- go run ./chats/
-gnome-terminal -- go run ./notifications/
-gnome-terminal -- go run ./messages/ 
+fuser -n tcp -k 8001 ; go run ./users &
+fuser -n tcp -k 8002 ; go run ./chats &
+fuser -n tcp -k 8003 ; go run ./notifications &
+fuser -n tcp -k 8004 ; go run ./messages
